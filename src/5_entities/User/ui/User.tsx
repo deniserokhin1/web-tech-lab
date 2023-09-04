deb80551188e446c5e58e2c85c35a466efe9a010
Denis <>
2023-09-04 08:32:28 +0300

    Update screenshots

--- src/5_entities/User/ui/User.tsx
+++ src/5_entities/User/ui/User.tsx
@@ -1,7 +1,7 @@
-import cls from './User.module.scss'
+// import cls from './User.module.scss'
 import type { FC } from 'react'
 import { classNames } from '6_shared/lib'
-import { useTranslation } from 'react-i18next'
+// import { useTranslation } from 'react-i18next'
 
 interface UserProps {
     className?: string
@@ -11,8 +11,8 @@ export const User: FC<UserProps> = (props) => {
     const { className } = props
     const mods = {}
 
-    const namespace = __IS_DEV__ ? 'translation' : 'namespace'
-    const { t } = useTranslation(namespace)
+    // const namespace = __IS_DEV__ ? 'translation' : 'namespace'
+    // const { t } = useTranslation(namespace)
 
-    return <div className={classNames(cls.container, mods, [className])}></div>
+    return <div className={classNames('', mods, [className])}></div>
 }

6702e3142b7d5a950ef2dd477ef341f95c7e49ae
Denis <oterohina@gmail.com>
2023-10-04 16:51:51 +0300

    add generate slice

---
+++ scripts/createSlice/templates/componentTemplate.js
@@ -0,0 +1,23 @@
+const interfaceConst = 'interface'
+
+module.exports = (
+    componentName,
+) => `import { classNames } from '6_shared/lib/classNames/classNames'
+import { useTranslation } from 'react-i18next'
+import cls from './${componentName}.module.scss'
+import { memo } from 'react'
+
+const namespace = __IS_DEV__ ? 'translation' : 'namespace'
+
+${interfaceConst} ${componentName}Props {
+    className?: string
+}
+
+export const ${componentName} = memo((props: ${componentName}Props) => {
+    const { className } = props
+    const { t } = useTranslation(namespace)
+    
+    return (
+        <div className={classNames(cls.container, {}, [className])}></div>
+    )
+})`
---
+++ scripts/createSlice/templates/storyTemplate.js
@@ -0,0 +1,26 @@
+module.exports = (layer, componentName) => `import type { Meta, StoryObj } from '@storybook/react'
+import { Theme } from '1_app/providers/ThemeProvider'
+import { ThemeDecorator } from '6_shared/config/storybook/Decorators/ThemeDecorator'
+import { ${componentName} } from './${componentName}'
+
+const meta = {
+    title: '${layer}/${componentName}',
+    component: ${componentName},
+    parameters: {
+        layout: 'fullscreen',
+    },
+    args: {},
+} satisfies Meta<typeof ${componentName}>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Light: Story = {
+    args: {},
+}
+
+export const Dark: Story = {
+    args: {},
+}
+Dark.decorators = [ThemeDecorator(Theme.DARK)]
+`
